791adf757809a6c3367f2388e9114e95660477e7
boni <boni@twine.hu>
2019-12-15 21:56:42 +0100

    Add forest

--- avatar.js
+++ avatar.js
@@ -39,6 +39,30 @@ avatar.add(leftHand);
 avatar.add(rightFoot);
 avatar.add(leftFoot);
 
+function makeTreeAt(x, z) {
+  var trunk = new THREE.Mesh(
+    new THREE.CylinderGeometry(50, 50, 200),
+    new THREE.MeshBasicMaterial({ color: 'sienna' })
+  );
+
+  var top = new THREE.Mesh(
+    new THREE.SphereGeometry(150),
+    new THREE.MeshBasicMaterial({ color: 'forestgreen' })
+  );
+
+  top.position.y = 175;
+  trunk.add(top);
+
+  trunk.position.set(x, -75, z);
+  scene.add(trunk);
+}
+
+makeTreeAt(0, 0);
+makeTreeAt(500, 0);
+makeTreeAt(-500, 0);
+makeTreeAt(750, -1000);
+makeTreeAt(-750, -1000);
+
 // Now, show what the camera sees on the screen:
 
 var isCartwheeling = false;
